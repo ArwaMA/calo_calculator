91b1b35f13ada0c7a90a03ebce13ded445cb96e0
ArwaMA <arwa.alamoudi.90@hotmail.com>
2021-10-18 17:39:35 +0000

    First Commit - Creating Calorie Calculator

---
+++ calo.py
@@ -0,0 +1,10 @@
+class calo:
+    
+    def __init__(self, gender, age, weight, height, active_level, goal):
+        '' 
+        self.gender = gender
+        self.age = age
+        self.weight = weight
+        self.height = height
+        self.active_level = active_level
+        self.goal = goal
